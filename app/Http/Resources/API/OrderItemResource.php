25c2588efc105f3111d6129a2f0860c6accbc96a
AbdelrahmanDeisty20 <abooda601@gmail.com>
2026-04-23 16:08:02 +0200

    feat: create API resource classes for Category, Order, and OrderItem models

--- app/Http/Resources/API/OrderItemResource.php
+++ app/Http/Resources/API/OrderItemResource.php
@@ -5,7 +5,7 @@ namespace App\Http\Resources\API;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class CategoryResource extends JsonResource
+class OrderItemResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
@@ -14,11 +14,13 @@ class CategoryResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return [
-            'id' => $this->id,
-            'name' => $this->name,
-            'image' => $this->image_path,
-            'sub_categories' => SubCategoryResource::collection($this->whenLoaded('subCategories')),
+        return[
+            "id" => $this->id,
+            "name" => $this->customer_name,
+            "price" => (float) ($this->unit_price),
+            "quantity" => $this->quantity,
+            "total_price" => $this->total_price,
+
         ];
     }
 }
--- app/Http/Resources/API/OrderResource.php
+++ app/Http/Resources/API/OrderResource.php
@@ -14,6 +14,15 @@ class OrderResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return [];
+        return [
+            'id' => $this->id,
+            'order_number' => $this->order_number,
+            'status' => $this->status,
+            'total_price' => (float) ($this->items->total_price),
+            'payment_method' => $this->payment_method,
+            'delivery_fees' => (float) ($this->governorate->shipping_cost ?? 0),
+            'items' => OrderItemResource::collection($this->whenLoaded('items')),
+            'created_at' => $this->created_at->format('Y-m-d H:i:s'),
+        ];
     }
 }
