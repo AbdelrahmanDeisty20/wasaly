61f026d2f2782f486c04435a06a816630da60b29
AbdelrahmanDeisty20 <abooda601@gmail.com>
2026-04-29 15:22:50 +0300

    feat: add ProductFilterRequest for validating product filtering and sorting parameters

--- app/Http/Requests/API/GENERAL/ProductFilterRequest.php
+++ app/Http/Requests/API/GENERAL/ProductFilterRequest.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Requests\API\GENERAL;
 
-use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Http\Exceptions\HttpResponseException;
 
 class ProductFilterRequest extends FormRequest
@@ -16,14 +16,15 @@ class ProductFilterRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'category_id'     => 'nullable|exists:sub_categories,id',
-            'min_price'       => 'nullable|numeric|min:0',
-            'max_price'       => 'nullable|numeric|min:0',
-            'special_offers'  => 'nullable|boolean',
-            'ratings'         => 'nullable|integer|in:1,2,3,4,5',
-            'sort'            => 'nullable|string|in:latest,min_price,max_price,offers',
+            'category_id' => 'nullable|exists:sub_categories,id',
+            'min_price' => 'nullable|numeric|min:0',
+            'max_price' => 'nullable|numeric|min:0',
+            'special_offers' => 'nullable|boolean',
+            'ratings' => 'nullable|integer|in:1,2,3,4,5',
+            'sort' => 'nullable|string|in:latest,min_price,max_price,offers',
         ];
     }
+
     public function messages(): array
     {
         return [
@@ -32,6 +33,7 @@ class ProductFilterRequest extends FormRequest
             'max_price.numeric' => __('messages.max_price_numeric'),
             'special_offers.boolean' => __('messages.special_offers_boolean'),
             'ratings.integer' => __('messages.ratings_integer'),
+            'ratings.in' => __('messages.ratings_in'),
             'sort.in' => __('messages.sort_in'),
         ];
     }
