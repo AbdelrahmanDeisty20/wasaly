cc71e295b0340c73725ef762156527bbc0d43aae
AbdelrahmanDeisty20 <abooda601@gmail.com>
2026-04-30 12:19:01 +0300

    feat: create ServiceResource for API response transformation

--- app/Http/Resources/API/GENERAL/ServiceResource.php
+++ app/Http/Resources/API/GENERAL/ServiceResource.php
@@ -10,11 +10,9 @@ class ServiceResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'id'         => $this->id,
-            'title' => $this->title,
-            'description' => $this->service_description,
-            'image' => $this->image_path,
-            
+            'id' => $this->id,
+            'name' => $this->service,
+            'price' => (float)$this->price,
         ];
     }
 }
