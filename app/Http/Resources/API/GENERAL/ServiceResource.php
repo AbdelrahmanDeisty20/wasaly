33bf8cf0d7e244f601ba0039feb4268a276d336b
AbdelrahmanDeisty20 <abooda601@gmail.com>
2026-04-30 12:27:13 +0300

    feat: create ServiceResource for API responses

--- app/Http/Resources/API/GENERAL/ServiceResource.php
+++ app/Http/Resources/API/GENERAL/ServiceResource.php
@@ -12,8 +12,6 @@ class ServiceResource extends JsonResource
         return [
             'id' => $this->id,
             'service' => $this->service,
-            'service_ar' => $this->service_ar,
-            'service_en' => $this->service_en,
             'price' => (float)$this->price,
         ];
     }
