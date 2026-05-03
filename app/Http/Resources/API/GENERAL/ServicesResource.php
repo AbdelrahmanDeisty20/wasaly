c4e9c7bf0d4c6095f94341a620d9ba303cead250
AbdelrahmanDeisty20 <abooda601@gmail.com>
2026-05-03 10:15:32 +0300

    feat: create ServicesResource for API service data transformation

--- app/Http/Resources/API/GENERAL/ServicesResource.php
+++ app/Http/Resources/API/GENERAL/ServicesResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources\API\GENERAL;
 
+use App\Http\Resources\API\SubCategoryResource;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -19,7 +20,7 @@ class ServicesResource extends JsonResource
             'title' => $this->title,
             'description' => $this->service_description,
             'image' => $this->image_path,
-            
+            'sub_category' => SubCategoryResource::collection($this->whenLoaded('subCategory')),
         ];
     }
 }
