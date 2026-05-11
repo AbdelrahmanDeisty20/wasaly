37ec6bbcc9876b8d97757a8f7a3f262925c36159
AbdelrahmanDeisty20 <abooda601@gmail.com>
2026-05-11 11:39:44 +0300

    feat: add ToggleServiceFavoriteRequest validation for service favoriting toggle

--- app/Http/Requests/API/GENERAL/ToggleServiceFavoriteRequest.php
+++ app/Http/Requests/API/GENERAL/ToggleServiceFavoriteRequest.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Requests\API\GENERAL;
 
-use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 
 class ToggleServiceFavoriteRequest extends FormRequest
@@ -12,18 +11,26 @@ class ToggleServiceFavoriteRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
      * Get the validation rules that apply to the request.
      *
-     * @return array<string, ValidationRule|array<mixed>|string>
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
      */
     public function rules(): array
     {
         return [
-            //
+            'service_id' => 'required|exists:services,id',
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'service_id.required' => __('messages.service_id_required'),
+            'service_id.exists' => __('messages.service_id_exists'),
         ];
     }
 }
