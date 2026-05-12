6451087098a9cc273dfd4b1c3b725e750b233997
AbdelrahmanDeisty20 <abooda601@gmail.com>
2026-05-12 12:09:27 +0300

    feat: create DayServices to retrieve and format available days via API resource

--- app/Services/API/General/DayServices.php
+++ app/Services/API/General/DayServices.php
@@ -12,7 +12,7 @@ class DayServices
     use ApiResponse;
     public function getDays()
     {
-        $days = AvailableDay::with('availableTimes')->get();
+        $days = AvailableDay::all();
         if($days->isEmpty()){
             return [
                 'status' => false,
@@ -23,7 +23,7 @@ class DayServices
         return [
             'status' => true,
             'message' => __('messages.days_fetched_successfully'),
-            'data' => AvailableDayResource::collection($days->load('availableTimes'))
+            'data' => AvailableDayResource::collection($days)
         ];
     }
 }
