cdd4a0409dc13308452ccca7fa183331600d408a
AbdelrahmanDeisty20 <abooda601@gmail.com>
2026-05-13 15:59:03 +0300

    feat: add SettingResource for API response formatting

--- app/Http/Resources/API/SettingResource.php
+++ app/Http/Resources/API/SettingResource.php
@@ -17,7 +17,7 @@ class SettingResource extends JsonResource
         return [
             'id' => $this->id,
             'key' => $this->key,
-            'value' => $this->value,
+            'value' => trim($this->value),
         ];
     }
 }
